8eae38eabc0a118b66592c81e7926d13d60fac15
Kavish Rizvi <rizvikavish205@gmail.com>
2021-10-08 16:24:54 +0530

    Thoughworks Incremental Activity

---
+++ src/match/CricketGame.java
@@ -0,0 +1,37 @@
+package match;
+
+import java.util.Scanner;
+
+import static match.Constant.*;
+
+//TODO: Refactor-Single Responsibility classes
+public class CricketGame {
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        CricketMatch match;
+
+        System.out.println("Input...");
+
+        System.out.println("Enter the Score to chase...:");
+        Integer targetRun = scan.nextInt();//12 for example
+
+        for (int playerSeq = STARTING_PLAYER; playerSeq < TOTAL_PLAYERS; playerSeq++) {
+
+            System.out.println("Enter Batsman Type : ");
+            Integer batsmanType = scan.nextInt();
+
+            match = new CricketMatch(targetRun, batsmanType);
+
+            String finalResult = match.playMatch();
+            if (finalResult.equals(LOST)) {
+                System.out.println(finalResult);
+            } else {
+                System.out.println(finalResult);
+            }
+        }
+    }
+
+}
+
---
+++ src/match/Run.java
@@ -0,0 +1,22 @@
+package match;
+
+import java.util.Random;
+
+import static match.Constant.*;
+
+public class Run {
+    public Integer calculateRun(int batsmanType) {
+        Random random = new Random();
+        if (batsmanType == 0) {
+            return random.nextInt(END_RANGE);
+        } else if (batsmanType == 1) {
+            return START_RANGE + random.nextInt((END_RANGE - START_RANGE) / 2) * 2;
+        } else {
+           int run = random.nextInt(END_RANGE_DEFENDERS);
+           if(run == 2) {
+               run += 1;
+           }
+           return run;
+        }
+    }
+}
